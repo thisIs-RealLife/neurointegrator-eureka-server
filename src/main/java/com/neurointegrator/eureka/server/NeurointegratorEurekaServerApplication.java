40402f595e14b7474a7719d7340b8c990ead5c25
soa <oleg.sorokin@best2pay.net>
2023-02-07 15:05:39 +0300

    init config

--- src/main/java/com/neurointegrator/eureka/server/NeurointegratorEurekaServerApplication.java
+++ src/main/java/com/neurointegrator/eureka/server/NeurointegratorEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.neurointegrator.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NeurointegratorEurekaServerApplication {
 
     public static void main(String[] args) {
