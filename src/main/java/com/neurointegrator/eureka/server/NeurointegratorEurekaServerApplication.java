dda1c99c7b12ea4e76f9555525d298e68797efa3
soa <oleg.sorokin@best2pay.net>
2023-02-07 14:53:21 +0300

    Initial commit

---
+++ src/main/java/com/neurointegrator/eureka/server/NeurointegratorEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.neurointegrator.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurointegratorEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeurointegratorEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neurointegrator/eureka/server/NeurointegratorEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurointegrator.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurointegratorEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
